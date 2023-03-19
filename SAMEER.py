49780addba658874793d3e30e00cc4b9916f59de
SAMEER HASHMI <116084722+SAMEER6363@users.noreply.github.com>
2023-03-19 21:47:43 +0500

    Update SAMEER.py

--- SAMEER.py
+++ SAMEER.py
@@ -1,4 +1,5 @@
 import os,platform,time
+os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
 print('Join My Facebook GROUP ');time.sleep(0.5)
 os.system('xdg-open https://www.facebook.com/groups/147435904593672/?ref=share ')
 print('\n\x1b[1;37m[●] Checking Update....✔️✔️');time.sleep(0.5)
