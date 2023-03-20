3c7dcf6d95b58a890aa1b5147eb7d356c1cc5a82
SAMEER HASHMI <116084722+SAMEER6363@users.noreply.github.com>
2023-03-20 22:05:15 +0500

    Create RUN.py

---
+++ RUN.py
@@ -0,0 +1,11 @@
+import os,platform
+os.system('git pull')
+print('FIRST JOIN MY FACEBOOK GROUP ')
+os.system('https://www.facebook.com/groups/147435904593672/?ref=share')
+print('   INSTALLATION MODULE ')
+os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+RUN=platform.architecture()[0]
+if RUN=="32bit":
+    print('Sorry 32 Bit Not Supported...')
+elif RUN=="64bit":
+    __import__("SAMEER")
